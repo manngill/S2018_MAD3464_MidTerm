5ed4f17d4bc0b4bc02b762c482d94c9eaf77e009
macstudent <macstudent@10.10.150.8>
2018-06-07 15:49:24 -0400

    Main class commit

---
+++ c0738175_MidTerm_MAD3464/src/c0738175_midterm_mad3464/C0738175_MidTerm_MAD3464.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0738175_midterm_mad3464;
+
+/**
+ *
+ * @author macstudent
+ */
+public class C0738175_MidTerm_MAD3464 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Enter String ");
+       reverse r1 = new reverse;
+       r1.display();
+    }
+    
+}
