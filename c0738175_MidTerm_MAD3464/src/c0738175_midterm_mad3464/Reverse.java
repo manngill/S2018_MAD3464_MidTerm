2665ce601ee672086d19c89305dc459ec1480a51
macstudent <macstudent@10.10.150.8>
2018-06-07 16:52:09 -0400

    SOP

--- c0738175_MidTerm_MAD3464/src/c0738175_midterm_mad3464/Reverse.java
+++ c0738175_MidTerm_MAD3464/src/c0738175_midterm_mad3464/Reverse.java
@@ -11,66 +11,18 @@ package c0738175_midterm_mad3464;
  * @author macstudent
  */
 public class Reverse {
-    
-   String input = "Lambton";
-
     public Reverse()
     {
         
     }
-    char[] temp = input.toCharArray();
-        int i = 0 , j =0;
-        int j = temp.length-1;
-        
-
-    public String getInput() {
-        return input;
-    }
-
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public char[] getTemp() {
-        return temp;
-    }
-
-    public void setTemp(char[] temp) {
-        this.temp = temp;
-    }
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
-    }
-
-    public int getJ() {
-        return j;
-    }
-
-    public void setJ(int j) {
-        this.j = j;
-    }
-       
-                
-               
-                
-        
-    for (i=0; i < j ; i++ )
-        {
-            
-            char t = temp[i];
-            temp[i] = temp[j];
-            temp[j]=t;
-        }
- 
-        public void display()
-        {
-            System.out.println();
-        }
-       
-    
-}
+  public String reverse(String s)
+  {
+   char r[] = s.toCharArray();
+   
+   for (int i = r.length -1;i>=0;i++)
+   {
+       System.out.println(r[i]);
+   }
+  return null;
+  }
+  }
