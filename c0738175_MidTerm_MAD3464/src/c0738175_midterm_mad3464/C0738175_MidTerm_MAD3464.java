c4dc936de5f704108c3844ff8670bcd1e4dc71cf
macstudent <macstudent@10.10.150.8>
2018-06-07 16:57:35 -0400

    new

--- c0738175_MidTerm_MAD3464/src/c0738175_midterm_mad3464/C0738175_MidTerm_MAD3464.java
+++ c0738175_MidTerm_MAD3464/src/c0738175_midterm_mad3464/C0738175_MidTerm_MAD3464.java
@@ -15,14 +15,11 @@ public class C0738175_MidTerm_MAD3464 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Enter String ");
-       ReverseString r1 = new ReverseString;
-       r1.display();
+      lambtoncollege R = new lambtoncollege();
+      R.reverse("lambton");
        
    
-       BinaryToDecimal input = new BinaryToDecimal();
-       System.out.print("Enter a binary number: ");
-       
+     
     }
     
 }
