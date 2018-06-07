552936b45bf98a29916520beb85fa7abc417bbf1
macstudent <macstudent@10.10.150.8>
2018-06-07 16:04:40 -0400

    Binary To Decimal

---
+++ c0738175_MidTerm_MAD3464/src/c0738175_midterm_mad3464/BinaryToDecimal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0738175_midterm_mad3464;
+import c0738175_midterm_mad3464.BinaryToDecimal;
+/**
+ *
+ * @author macstudent
+ */
+public class BinaryToDecimal {
+    public static void main(String args[]){
+       BinaryToDecimal input = new BinaryToDecimal();
+       System.out.print("Enter a binary number: ");
+       
+    
+}
+    
+}
